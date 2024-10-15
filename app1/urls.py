9e5d8b5da0d745db971ad65a6c054c610af02a0e
Kushal914 <kushal.thapa914@gmail.com>
2024-10-15 18:01:33 +0545

    home shows featured categories and food items, clicking Food Items on a category shows food items only belonging to the category

--- app1/urls.py
+++ app1/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name = 'home'),
+    path('categories/', views.categories, name='categories'),
     path('foods/', views.foods, name = 'foods'),
+    path('category-foods/<int:category_id>/', views.category_foods, name='category_foods'),
 ]
--- app1/views.py
+++ app1/views.py
@@ -1,11 +1,21 @@
 from django.shortcuts import render
-from .models import Food
+from .models import Food, Category
 
 # Create your views here.
 
 def home(request):
-    return render(request, 'home.html')
+    category_items = Category.objects.filter(is_active=True, is_featured=True)
+    food_items = Food.objects.filter(is_active=True, is_featured=True)
+    return render(request, 'home.html', {'category_items': category_items, 'food_items' : food_items})
+
+def categories(request):
+    category_items = Category.objects.filter(is_active=True)
+    return render(request, 'categories.html', {'category_items': category_items})
 
 def foods(request):
-    food = Food.objects.all()
-    return render(request, 'foods.html', {'foods' : food})
+    food_items = Food.objects.filter(is_active=True)
+    return render(request, 'foods.html', {'food_items' : food_items})
+
+def category_foods(request, category_id):
+    food_items = Food.objects.filter(category__id=category_id, is_active=True)
+    return render(request, 'foods.html', {'food_items' : food_items})
